1c252207a1b3bdce59b9572d9f5bf6d8da9a29d3
Xythium <xythium@gmail.com>
2021-07-04 22:58:13 +0200

    1.16.9: use correct artist concatenation on beatport

--- Common/Beatport/BeatportUtils.cs
+++ Common/Beatport/BeatportUtils.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeatportApi;
+using BeatportApi.Beatport;
+using Discord;
+using Raven.Client;
+using Serilog;
 
 namespace Common.Beatport
 {
@@ -45,5 +53,99 @@ namespace Common.Beatport
         }
 
         public static IdFromUrlResult GetIdFromUrl(string url) { return GetIdFromUrl(new Uri(url)); }
+
+        public static async Task<EmbedBuilder> EmbedBuilder(BeatportApi.Beatport.Beatport api, BeatportRelease release, IDocumentSession session)
+        {
+            var tracks = await BeatportDbUtils.GetTracksOrCache(api, session, release.TrackUrls);
+
+            var sb1 = new StringBuilder();
+            var sb2 = new StringBuilder();
+            var sb3 = new StringBuilder();
+
+            foreach (var track in tracks)
+            {
+                var line = FormatTrack(track);
+
+                if (sb2.Length + line.Length >= 1023)
+                {
+                    sb3.AppendLine(line);
+                }
+                else if (sb1.Length + line.Length >= 1023)
+                {
+                    sb2.AppendLine(line);
+                }
+                else
+                {
+                    sb1.AppendLine(line);
+                }
+            }
+
+            var color = ColorUtils.MostCommonColor(release.Image.DynamicUri.Replace("{w}", "1400")
+                .Replace("{h}", "1400"));
+
+            Log.Information($"embed color: {color} {color:X8}");
+            var artist = release.ArtistConcat;
+
+            if (release.Artists.Count > 9 || artist.Length > 128)
+                artist = "Various Artists";
+
+            var embed = new EmbedBuilder().WithTitle($"{artist} - {release.Name}")
+                //.WithColor(color)
+                .WithUrl($"https://www.beatport.com/release/{release.Slug}/{release.Id}")
+                .WithThumbnailUrl(release.Image.DynamicUri.Replace("{w}", "1400")
+                    .Replace("{h}", "1400"))
+                .AddField("Release Date", release.NewReleaseDate.ToString("yyyy-MM-dd"), true)
+                .AddField("UPC", release.Upc, true)
+                .AddField("Catalog", release.CatalogNumber ?? "(none)", true)
+                .AddField("Tracklist", sb1.ToString());
+
+            if (!string.IsNullOrWhiteSpace(release.Description))
+                embed = embed.WithDescription(release.Description);
+            return embed;
+        }
+
+        public static string FormatTrack(BeatportTrack track, bool includeArtist = false)
+        {
+            var featureList = new List<string>
+            {
+                TimeSpan.FromMilliseconds(track.LengthMs)
+                    .ToString("m':'ss")
+            };
+
+            if (track.Bpm != null && track.Bpm != 0)
+            {
+                featureList.Add(track.Bpm.ToString());
+            }
+
+            if (track.Key != null)
+            {
+                featureList.Add(track.Key.Name);
+            }
+
+            if (track.Isrc != null)
+            {
+                featureList.Add(track.Isrc);
+            }
+
+            var name = track.Name;
+
+            if (!string.IsNullOrWhiteSpace(track.MixName) && track.MixName != "Original Mix")
+            {
+                name = $"{track.Name} ({track.MixName})";
+            }
+
+            var sb = new StringBuilder();
+
+            if (includeArtist)
+            {
+                sb.Append($"{track.Number}. {track.ArtistConcat} - {name} [{string.Join(", ", featureList)}]");
+            }
+            else
+            {
+                sb.Append($"{track.Number}. {name} [{string.Join(", ", featureList)}]");
+            }
+
+            return sb.ToString();
+        }
     }
 }
